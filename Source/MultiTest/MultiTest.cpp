d1f869c16c15b4b2cc1befa008ee7b55ae6e7ecc
Kieran Mignon <kieranmignon32@gmail.com>
2023-02-08 10:55:30 +0100

    Build with C++ from VS2022

---
+++ Source/MultiTest/MultiTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MultiTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiTest, "MultiTest" );
